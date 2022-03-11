6ca5e333761b631ac0aafd4b935c632d5ec4747e
Ashish <a.sankpal@aexonic.com>
2022-03-11 11:36:21 +0530

    industry banking content

--- pages/blog.js
+++ pages/blog.js
@@ -43,7 +43,7 @@ export const getStaticProps = async () => {
 }
 export function Blog({ data }) {
 
-    console.log(data)
+    // console.log(data)
     const [show, setShow] = React.useState();
 
     return (
