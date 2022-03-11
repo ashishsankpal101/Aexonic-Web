5f133e2c60bf1c4f2351d7c16e7055d21a396925
Ashish <a.sankpal@aexonic.com>
2022-03-11 13:04:33 +0530

    Working on AOS issu

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.scss'
 import '../public/scss/style.scss'
-
+import React, { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init();
+  })
   return <Component {...pageProps} />
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React, { useEffect } from "react";
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
+        
     }
 
     render() {
